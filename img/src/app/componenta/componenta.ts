d8d58d5af10831b03608feed8f0340b14094c841
Olek <g00467581@atu.ie>
2026-02-21 16:21:45 +0000

    Add files via upload

---
+++ img/src/app/componenta/componenta.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-a',
+  imports: [],
+  templateUrl: './componenta.html',
+  styleUrl: './componenta.css',
+})
+export class Componenta {
+
+}
---
+++ img/src/app/componentb/componentb.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-b',
+  imports: [],
+  templateUrl: './componentb.html',
+  styleUrl: './componentb.css',
+})
+export class Componentb {
+
+}
